65cab898060b7bebe3dbb89301521356de43f723
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-04-19 18:38:49 +0100

    Updated the projects

--- src/data.js
+++ src/data.js
@@ -1,81 +1,90 @@
-export const projects =[
-    {
-        title: "DAO Dapp",
-        subtitle: "A DAO Sample project",
-        description:
-            "A DAO sample application to enable participants create and vote on Proposals",
-        image: "./2.svg",
-        link: "https://building-a-dao-bice.vercel.app/"
-    },
-    {
-        title: "Whitelist-Dapp",
-        subtitle: "Whitelist Dapp",
-        description: 
-            "A fully functional Decentralized Application for adding early adopters to the Whitelist",
-        image: "./crypto-devs.svg",
-        link: "https://whitelist-dapp-ten-fawn.vercel.app/",
-    },
-    {
-        title: "NFT-Collection",
-        subtitle: "An NFT-Collection sample project",
-        description:
-            "An NFT Collection Dapp for minting NFTs",
-        image: "./5.svg",
-        link: "https://nft-collection-omega-ten.vercel.app/",
-    },
-    {
-        title: "Initial Coin Offering",
-        subtitle: "An ICO Dapp ",
-        description: 
-            "An ICO Dapp which allows NFT holds to mint tokens and convert their NFTs to native tokens of the Dapp",
-        image: "./0.svg",
-        link: "https://ico-xi.vercel.app/"
-    }
+export const projects = [
+  {
+    title: "DeFi Exchange",
+    subtitle: "A DeFi Token Swap",
+    description:
+      "A Decentralized Token Exchange app for swapping between tokens just like UniSWap",
+    image: "./8.svg",
+    link: " https://de-fi-exchange-nine.vercel.app/",
+  },
+
+  {
+    title: "DAO Dapp",
+    subtitle: "A DAO Sample project",
+    description:
+      "A DAO sample application to enable participants create and vote on Proposals",
+    image: "./2.svg",
+    link: "https://building-a-dao-bice.vercel.app/",
+  },
+  {
+    title: "Whitelist-Dapp",
+    subtitle: "Whitelist Dapp",
+    description:
+      "A fully functional Decentralized Application for adding early adopters to the Whitelist",
+    image: "./crypto-devs.svg",
+    link: "https://whitelist-dapp-ten-fawn.vercel.app/",
+  },
+  {
+    title: "NFT-Collection",
+    subtitle: "An NFT-Collection sample project",
+    description: "An NFT Collection Dapp for minting NFTs",
+    image: "./5.svg",
+    link: "https://nft-collection-omega-ten.vercel.app/",
+  },
+  {
+    title: "Initial Coin Offering",
+    subtitle: "An ICO Dapp ",
+    description:
+      "An ICO Dapp which allows NFT holds to mint tokens and convert their NFTs to native tokens of the Dapp",
+    image: "./0.svg",
+    link: "https://ico-xi.vercel.app/",
+  },
 ];
 
 export const skills = [
-
-        'Solidity',
-        'Rust -Beginner',
-        'EtherJs',
-        'Web3.js',
-        'AWS',
-        'JavaScript', 
-        'Python',
-        'React',
-        'NodeJs',
-        'NextJS',
-        'GraphQL',
-        'Severless',
-        'Alchemy',
-        'Ethereum',
-
+  "Solidity",
+  "Rust -Beginner",
+  "EtherJs",
+  "Web3.js",
+  "AWS",
+  "JavaScript",
+  "Python",
+  "React",
+  "NodeJs",
+  "NextJS",
+  "GraphQL",
+  "Severless",
+  "Alchemy",
+  "Ethereum",
 ];
 
 export const testimonials = [
-    {
-        quote: 'It was amazing working with Sunday. He delivers on time and according to our specification.',
-        image: '',
-        name: 'Ali',
-        company: 'PrimeLabs.'
-    },
-    {
-        quote: 'Sunday is an awesome Engineer to work with. He ensures he understands what his clients wants and delivers on time',
-        image: '',
-        name: 'Ahmed',
-        company: 'PrimeLabs'
-    },
-    {
-        quote: 'I am Co-Founding a Decentralized on-chain Hotel reservation platform with Sunday. He figured out the value chain and the differentiating factors and builds that into the smart contracts. This dude is amazing to work with.',
-        image: '',
-        name: 'Tomya',
-        company: 'KripBooking'
-    },
-    {
-        quote: 'Working with Sunday gives you comfort to receive great finished products. I am part of the team at KripBooking, Sunday is an amazing Team Leader to work with',
-        image: '',
-        name: 'Pranjil',
-        company: 'KripBooking'
-    },
-
+  {
+    quote:
+      "It was amazing working with Sunday. He delivers on time and according to our specification.",
+    image: "",
+    name: "Ali",
+    company: "PrimeLabs.",
+  },
+  {
+    quote:
+      "Sunday is an awesome Engineer to work with. He ensures he understands what his clients wants and delivers on time",
+    image: "",
+    name: "Ahmed",
+    company: "PrimeLabs",
+  },
+  {
+    quote:
+      "I am Co-Founding a Decentralized on-chain Hotel reservation platform with Sunday. He figured out the value chain and the differentiating factors and builds that into the smart contracts. This dude is amazing to work with.",
+    image: "",
+    name: "Tomya",
+    company: "KripBooking",
+  },
+  {
+    quote:
+      "Working with Sunday gives you comfort to receive great finished products. I am part of the team at KripBooking, Sunday is an amazing Team Leader to work with",
+    image: "",
+    name: "Pranjil",
+    company: "KripBooking",
+  },
 ];
